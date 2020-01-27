9508c1e1112402e2feeb84f846c8f43ff443677f
joseph.olstad <joseph.olstad@1321830.no-reply.drupal.org>
2020-01-27 00:20:20 -0500

    perms thing.

--- post_install.sh
+++ post_install.sh
@@ -6,6 +6,8 @@ if [ ! -f html/sites/default/settings.php ]; then
   printf "Creating your settings.php file\n";
   sudo chmod 775 html/sites/default;
   cp html/sites/default/default.settings.php html/sites/default/settings.php
+  sudo chmod 664 html/sites/default/settings.php
+  sudo chown --reference=. html/sites/default/settings.php
   sudo mkdir html/sites/default/files
   sudo chmod 775 html/sites/default/files
 fi
