4b80b6dfe5a29a8cb5f08dbd5eaf7b2753b15428
olstjos <j@7pro.ca>
2020-03-03 17:11:10 -0500

    Functionality is 'almost' working, but not yet.

--- custom/modules/agri_admin/src/AgriAdminHelper.php
+++ custom/modules/agri_admin/src/AgriAdminHelper.php
@@ -3,7 +3,7 @@
 namespace Drupal\agri_admin;
 
 //use Drupal\something\AgriUtils;
-use Drupal\node\Entity;
+use Drupal\node\Entity\Node;
 
 
 class AgriAdminHelper {
@@ -12,19 +12,34 @@ class AgriAdminHelper {
     \Drupal::messenger()->addMessage($message);
   }
 
+
   static public function addToLog($message) {
     \Drupal::logger('agri_admin')->notice($message);
   }
 
+
   static public function postUpdateProcess($action, $entity_id, $bundle) {
     static::addToLog('test DEBUG');
     static::addToLog($action . ' entity_id ' . $entity_id);
-    static::findParentOfNid($entity_id, 'main', $parentUuid);
+    $parentUuid = NULL;
+    $parentUuidClean = NULL;
+    if ($bundle == 'page') {
+      $parentNid = static::findParentOfNid($entity_id, 'main', $parentUuid, $parentUuidClean);
+      if ($parentNid > 0) {
+        $resultCode = static::createChildOfNid($entity_id, 'sidebar', $parentNid, $parentUuid);
+        if ($resultCode) {
+          static::addToLog('Successfully created new sidebar link for nid=id=' . $entity_id);
+        }
+      }
+    }
   }
+
 
   static public function getLang() {
     return \Drupal::languageManager()->getCurrentLanguage()->getId();
   }
+
+
   static public function getOtherLang() {
     // Get the list of all languages
     $langcode = static::getLang();
@@ -36,25 +51,44 @@ class AgriAdminHelper {
   }
 
 
-  static public function createChildOfNid($nid, $menu_name = 'sidebar', $parentUuid) {
+  static public function menuLinkExists($nid, $menu_name = 'sidebar') {
+    $menu = \Drupal::entityTypeManager()->getStorage('menu_link_content')
+      ->loadByProperties([
+        'link.uri' => 'entity:node/' . $nid,
+        'menu_name' => $menu_name,
+      ]);
+    $parentLink = reset($parentLink);
+    if (isset($parentLink)) {
+      static::addToLog('Menu Link Exists,<pre>id=' . print_r($parentLink->id(), TRUE) . '</pre>');
+      return TRUE;
+    }
+    static::addToLog('Sidebar menu link for nid does not yet exist: nid=<pre>' . print_r($nid, TRUE) . '</pre>');
+    return FALSE;
+  }
+
+  static public function createChildOfNid($nid, $menu_name = 'sidebar', $parentNid, $parentUuid) {
     // Load main navigation menu link for nid, find the parent nid, then look up the menu link
     // in the sidebar with that nid, that will be the parent of this new sidebar link.
 
     $node = Node::load($nid);
     $lang = static::getLang();
     if ($lang == 'en') {
-      $title = $node->getTitle();
-    
-      $menu_link = MenuLinkContent::create([
-        'title' => $title,
-        'link' => ['uri' => 'internal:/node/' . $nid],
-        'menu_name' => $menu_name,
-        'expanded' => true,
-        'langcode' => $lang,
-        'status' => TRUE,
-        'parent' => $parent,
-      ]);
+      if (!static::menuLinkExists($nid, $menu_name)) {
+        $title = $node->getTitle();
+
+        $menu_link = \Drupal\menu_link_content\Entity\MenuLinkContent::create([
+          'title' => $title,
+          'link' => ['uri' => 'entity:node/' . $nid],
+          'menu_name' => $menu_name,
+          'expanded' => true,
+          'langcode' => $lang,
+          'status' => TRUE,
+          'parent' => $parentUuid,
+        ]);
+        return $menu_link->save();
+      }
     }
+    return FALSE;
   }
 
 
@@ -89,8 +123,6 @@ class AgriAdminHelper {
             $parentParams = $parentLink->getUrlObject()->getRouteParameters();
             $parentLinkNodeId = $parentParams['node'];
             if (is_numeric($parentLinkNodeId)) {
-              //static::addToLog('parent menu link nid :' . $parentLinkNodeId);
-              //static::addToLog('success:' . $parentLinkNodeId);
               $parentUuid = $parentMenuLinkId;
               $parentUuidClean = $newUuid;
               return $parentLinkNodeId;
