6730161270798bc8225ff9bf4fbf558fe014524b
olstjos <j@7pro.ca>
2020-04-24 11:33:24 -0400

    agrcms/d8#69 - Fix bug that was affecting Jun. only translate the menu link if it is not already translated.

--- custom/modules/agri_admin/src/AgriAdminHelper.php
+++ custom/modules/agri_admin/src/AgriAdminHelper.php
@@ -613,19 +613,19 @@ class AgriAdminHelper {
     $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
     $result = $menu_link_manager->loadLinksByRoute('entity.node.canonical', ['node' => $nid]);
     $node = NULL;
-    if (count($result) > 0) {
-      $node = Node::load($nid);
-    }
     foreach ($result as $menu_item) {
       if (is_object($menu_item)) {
         $otherLang = static::getOtherLang();
         $id = $menu_item->getPluginDefinition()['metadata']['entity_id'];
         $menu_link = \Drupal::entityTypeManager()->getStorage('menu_link_content')->load($id);
-        if (isset($node) && !is_null($node)) {
-          if ($node->hasTranslation($otherLang)) {
-            $title = $node->getTranslation($otherLang)->getTitle();
-            $menu_link->addTranslation($otherLang, ['title' => $title]);
-            $menu_link->save();
+        if (!$menu_link->hasTranslation($otherLang)) {
+          $node = Node::load($nid);
+          if (isset($node) && !is_null($node)) {
+            if ($node->hasTranslation($otherLang)) {
+              $title = $node->getTranslation($otherLang)->getTitle();
+              $menu_link->addTranslation($otherLang, ['title' => $title]);
+              $menu_link->save();
+            }
           }
 	}
       }
