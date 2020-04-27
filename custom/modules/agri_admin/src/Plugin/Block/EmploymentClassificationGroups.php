de48a8e9cfd1bd32bf4cd381595aaf370f0558b1
olstjos <j@7pro.ca>
2020-04-27 15:52:46 -0400

    agrcms/d8#27 - Fix the custom block for employment classification groups.

--- custom/modules/agri_admin/src/Plugin/Block/EmploymentClassificationGroups.php
+++ custom/modules/agri_admin/src/Plugin/Block/EmploymentClassificationGroups.php
@@ -11,7 +11,6 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Block\BlockBase;
 use Drupal\node\Entity\Node;
 
- 
 
 /**
  * Provides an employment classification groups block.
@@ -19,102 +18,115 @@ use Drupal\node\Entity\Node;
  * @Block(
  *   id = "employment_classification_groups",
  *   admin_label = @Translation("Employment Classification Groups Display"),
- *   category = @Translation("Custom")
  * )
  */
 class  EmploymentClassificationGroups extends BlockBase {
 
-	protected function getNode() {
-		$node = \Drupal::routeMatch()->getParameter('node');
-		if ($node) {
-   		if (is_string($node) && is_numeric($node)) { 
+  protected function getNode() {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if ($node) {
+      if (is_string($node) && is_numeric($node)) {
         $nid = $node;
       }
-   	  else if (gettype($node) == 'object') {	
-        $nid = $node->id();    
+      else if (gettype($node) == 'object') {
+        $nid = $node->id();
       }
-		  if ($node->getType() == 'empl') {
-		     return $node ;
-		  }	
-		}
+      if ($node->getType() == 'empl') {
+        return $node;
+      }
+    }
     return false;
-	}
+  }
 
-	/**
-   	* {@inheritdoc}
-   	*/
-	public function build() {
-	
-//		$config          = $this->getConfiguration();
+ /**
+  * {@inheritdoc}
+  */
+  public function build() {
     $config = $this->getConfiguration(); 
-    $block['#id'] = $config['id']; 
-    $block['#title'] = 'Employment Classification Groups Display'; 
+    $block['#id'] = $config['id'];
+    $block['#title'] = 'Employment Classification Groups';
     $block['content']['results']['#type'] = 'markup';
 
-		$current_language = Drupal::languageManager()->getCurrentLanguage();
-		$currentlangId    = $current_language->getId();
-	  $classification   = '' ;
+    $current_language = Drupal::languageManager()->getCurrentLanguage();
+    $currentlangId    = $current_language->getId();
+    $classification   = '';
 
     $emplNode =  $this->getNode();
-    if (!$emplNode){
-	     $block['content']['#markup'] = $classification ;
-	     return $block;
+    if (!$emplNode) {
+      $block['content']['#markup'] = $classification;
+      return $block;
     }
-		//$emplNode = Node::load(3052);
 
-		if ($paragraph_field_items = $emplNode->get('field_classification')->getValue()) {
-		  // Get storage. It very useful for loading a small number of objects.
-		  $paragraph_storage = \Drupal::entityTypeManager()->getStorage('paragraph');
-		  // Collect paragraph field's ids.
-		  $ids = array_column($paragraph_field_items, 'target_id');
-		  // Load all paragraph objects.
-		  $paragraphs_objects = $paragraph_storage->loadMultiple($ids);
-		  /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */ 
-		  foreach ($paragraphs_objects as $paragraph) {
-			// Get field from the paragraph.
-			$groupval    = $paragraph->get('field_class_id')->value;
-			$subgroupval = $paragraph->get('field_class_sub')->value;
-			$levelval    = $paragraph->get('field_class_level')->value;
-			$tmpText     = '';
-			// Do something with $text...
-			if ($subgroupval){ 
-			       	$tmpText = $groupval.'-'.$subgroupval.'-'.$levelval;	}
-			else{   $tmpText = $groupval.'-'.$levelval;	                }
-			
-			if ( strlen($classification) >0 ) {
-			       	if ($currentlangId == 'en') {
-                                    $classification = $classification.' or '. $tmpText;      }
-                                else {   $classification = $classification.' ou '. $tmpText; }
-                          } else {   $classification = $tmpText;	                     }     
-		  }
-		  
-		  // get equivalent flag value
-		  $equivalentval = $emplNode->get('field_and_equivalent')->getValue() ;
-		  $equivalentlbl = $emplNode->get('field_and_equivalent')->getFieldDefinition()->getLabel();
-		  if ($equivalentval) { 
-		      $classification = $classification.' '.$equivalentlbl ;
-		  }
-	     }	
+    if ($paragraph_field_items = $emplNode->get('field_classification')->getValue()) {
+      // Get storage. It very useful for loading a small number of objects.
+      $paragraph_storage = \Drupal::entityTypeManager()->getStorage('paragraph');
+      // Collect paragraph field's ids.
+      $ids = array_column($paragraph_field_items, 'target_id');
+      // Load all paragraph objects.
+      $paragraphs_objects = $paragraph_storage->loadMultiple($ids);
+      /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */
+      foreach ($paragraphs_objects as $paragraph) {
+        // Get field from the paragraph.
+        $groupval    = $paragraph->get('field_class_id')->value;
+        $subgroupval = $paragraph->get('field_class_sub')->value;
+        $levelval    = $paragraph->get('field_class_level')->value;
+        $tmpText     = '';
+        // Do something with $text...
+        if ($subgroupval) {
+          $tmpText = $groupval.'-'.$subgroupval.'-'.$levelval;
+        }
+        else {
+          $tmpText = $groupval.'-'.$levelval;
+        }
 
-	    $classification = '<div class="field--label">Classification</div>'.'<div class="field--items">'.$classification.'</div>';
+        if ( strlen($classification) >0 ) {
+          if ($currentlangId == 'en') {
+            $classification = $classification.' or '. $tmpText;
+          }
+          else {
+            $classification = $classification.' ou '. $tmpText;
+          }
+        }
+        else {
+          $classification = $tmpText;
+        }
+      }
 
-	     $block['content']['#markup'] = $classification ;
-	     return $block;
-	}	
+      // get equivalent flag value
+      $equivalentval = $emplNode->get('field_and_equivalent')->getValue();
+      $equivalentlbl = $emplNode->get('field_and_equivalent')->getFieldDefinition()->getLabel();
+      if ($equivalentval) {
+        $classification = $classification.' '.$equivalentlbl;
+      }
+    }
 
-	/**
-   	* {@inheritdoc}
-   	*/
-	public function blockForm($form, FormStateInterface $formState) {
-	    $config = $this->getConfiguration();
-	    return $form;
-	}	
+    $classification = '<div class="field--items">'.$classification.'</div>';
+
+    $block['content']['#markup'] = $classification;
+    return $block;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'access content');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $formState) {
+    $config = $this->getConfiguration();
+    return $form;
+  }
 	
-	/**
-	* {@inheritdoc}
-	*/
-	public function blockSubmit($form, FormStateInterface $formState) {
-		$this->configuration['employment_classification_groups'] =
-			$formState->getValue('employment_classification_groups');
-	}
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $formState) {
+    $this->configuration['employment_classification_groups'] =
+    $formState->getValue('employment_classification_groups');
+  }
+
 }
