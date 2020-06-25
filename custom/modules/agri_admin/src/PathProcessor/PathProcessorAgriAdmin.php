6f3fab2b2ec5f818d8af39a336c39da5e7b4d8bb
olstjos <j@7pro.ca>
2020-06-25 12:25:05 -0400

    agrcms/d8#27 - Fix for the caching of preview pages, add a unixtimestamp in the get parameter options for full and teaser preview pages.

---
+++ custom/modules/agri_admin/src/PathProcessor/PathProcessorAgriAdmin.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\agri_admin\PathProcessor;
+
+use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
+use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Path processor to bypass caching on anonymous node preview.
+ */
+class PathProcessorAgriAdmin implements InboundPathProcessorInterface, OutboundPathProcessorInterface {
+
+  public $unixtime=null;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processInbound($path, Request $request) {
+    return $path;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processOutbound($path, &$options = [], Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
+    if (strpos($path, 'preview') > 0 && strpos($path, 'node') > 0) {
+      if (strpos($path, 'full') > 0 || strpos($path, 'teaser') > 0) {
+        if (!isset($this->unixtime)) {
+          $this->unixtime = time();
+        }
+        $options['query']['t'] = $this->unixtime;
+      }
+    }
+    return $path;
+  }
+
+}
