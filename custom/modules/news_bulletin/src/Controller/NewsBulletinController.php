456612de6ef64093f1c27299fa8ffb768a34fd5b
olstjos <j@7pro.ca>
2020-07-23 12:04:27 -0400

    agrcms/d8#42 - Add ajax methods (setter) and getter(for testing).

--- custom/modules/news_bulletin/src/Controller/NewsBulletinController.php
+++ custom/modules/news_bulletin/src/Controller/NewsBulletinController.php
@@ -6,8 +6,120 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\views\Views;
 use Drupal\agri_admin\AgriAdminHelper;
+use Drupal\user\PrivateTempStoreFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class NewsBulletinController extends ControllerBase {
+
+  protected $tempStore;
+
+  // Pass the dependency to the object constructor
+  public function __construct(PrivateTempStoreFactory $temp_store_factory) {
+    // For "news_bulletin," any unique namespace will do
+    $this->tempStore = $temp_store_factory->get('news_bulletin');
+  }
+
+  // Uses Symfony's ContainerInterface to declare dependency to be passed to constructor
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('user.private_tempstore')
+    );
+  }
+
+  public function setTempConfig() {
+    $lang = \Drupal::languageManager()->getCurrentLanguage()->getId();
+
+    // TODO: Look this up
+    $langseg = [
+      'en' => 'en',
+      'fr' => 'fr',
+    ];
+
+    // Handle array() parameter like [].
+    $news_type_weights = \Drupal\Component\Utility\Xss::filter(\Drupal::request()->query->get('tids'));
+    if (!is_array($news_type_weights) || (!isset($news_type_weights) || empty($news_type_weights))) {
+      $news_type_weights = [];
+    }
+
+    // Handle comma seperated param and turn it into an array.
+    if (empty($news_type_weights) && isset($_GET['tids'])) {
+      foreach (explode(',', $_GET['tids']) as $tid) {
+        if ($tid == (int) $tid) {
+          $news_type_weights[] = $tid;
+        }
+      }
+    }
+
+    // Handle array() parameter like [].
+    $news_nids = \Drupal\Component\Utility\Xss::filter(\Drupal::request()->query->get('nids'));
+    if (!is_array($news_nids) || (!isset($news_nids) || empty($news_nids))) {
+      $news_nids = [];
+    }
+
+    // Handle comma seperated param and turn it into an array.
+    if (isset($_GET['nids'])) {
+      foreach (explode(',', $_GET['nids']) as $nid) {
+        if ($nid == (int) $nid) {
+          $news_nids[] = $nid;
+        }
+      }
+    }
+
+    if (empty($news_type_weights) && empty($news_nids)) {
+      return new JsonResponse(['status' => TRUE, 'message' => ['no changes']]);
+    }
+    if (!empty($news_type_weights) && !empty($news_nids)) {
+      $this->setTypeWeights($news_type_weights);
+      $this->setNewsNids($news_nids);
+      return new JsonResponse(['status' => TRUE, 'message' => ['node selection and term weights updated']]);
+    }
+    if (!empty($news_type_weights)) {
+      $this->setTypeWeights($news_type_weights);
+      return new JsonResponse(['status' => TRUE, 'message' => ['term weights updated']]);
+    }
+    if (!empty($news_nids)) {
+      $this->setNewsNids($news_nids);
+      return new JsonResponse(['status' => TRUE, 'message' => ['term weights updated']]);
+    }
+    return new JsonResponse(['status' => FALSE, 'message' => ['This should not occur']]);
+  }
+
+  public function getTempConfig() {
+      return new JsonResponse(['status' => TRUE, 'message' => ['This is the temp config'], 'data' => [$this->getTempValues()]]);
+  }
+
+
+  // Save some temporary data (terms selected and their order/weight (is important)).
+  public function setTypeWeights($terms_and_order_or_weights) {
+    $this->tempStore->set('news_type_weights', $terms_and_order_or_weights);
+  }
+
+  // Save some temporary data (nids selected).
+  public function setNewsNids($news_nids_selected) {
+    $this->tempStore->set('news_nids', $news_nids_selected);
+  }
+
+  // Read some temporary data
+  public function getTypeWeights() {
+    $news_type_weights = $this->tempStore->get('news_type_weights');
+    // Do other stuff, return a render array, etc...
+  }
+
+  // Read some temporary data
+  public function getTempValues() {
+    $news_type_weights = $this->tempStore->get('news_type_weights');
+    if (!isset($news_type_weights)) {
+      $news_type_weights = array();
+    }
+    $news_nids_selected = $this->tempStore->get('news_nids');
+    if (!isset($news_nids_selected)) {
+      $news_nids_selected = array();
+    }
+    $config_array = ['tids' => $news_type_weights, 'nids' => $news_nids_selected];
+    return $config_array;
+  }
+
+
   /**
    * Display the markup.
    *
