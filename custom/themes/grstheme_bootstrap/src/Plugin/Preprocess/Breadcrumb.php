7c04d468a7a7c636928c5c970d8790c5bb1c0db4
olstjos <j@7pro.ca>
2020-10-08 14:53:48 -0400

    agrcms/d8#271 - Fix regression again, this time for revert revision.

--- custom/themes/grstheme_bootstrap/src/Plugin/Preprocess/Breadcrumb.php
+++ custom/themes/grstheme_bootstrap/src/Plugin/Preprocess/Breadcrumb.php
@@ -49,7 +49,7 @@ class Breadcrumb extends BootstrapBreadcrumb {
       $route_match = \Drupal::routeMatch();
       $page_title = \Drupal::service('title_resolver')->getTitle($request, $route_match->getRouteObject());
       $node = \Drupal::routeMatch()->getParameter('node');
-      if (isset($node) && !is_string($node) && is_object($node) && method_exists($node, 'getType') {
+      if (isset($node) && !is_string($node) && is_object($node) && method_exists($node, 'getType')) {
         $nodetype= $node->getType();
         if ($nodetype == 'page' || $nodetype == 'landing_page') {
           if (!empty($page_title)) {
