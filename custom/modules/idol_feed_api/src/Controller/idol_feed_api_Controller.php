baf3cbe2d8b7e156bb36eec1b5a616ce55afbddb
jun.hng <jun.hng@gmail.com>
2020-10-23 21:48:59 -0400

    d8#75 - update idol api to handle page interne, opportunite d'emploi and additional & sign

--- custom/modules/idol_feed_api/src/Controller/idol_feed_api_Controller.php
+++ custom/modules/idol_feed_api/src/Controller/idol_feed_api_Controller.php
@@ -55,16 +55,18 @@ class idol_feed_api_Controller extends ControllerBase {
           // $nodeurl = url(drupal_get_path_alias('node/' . $node->nid), array('absolute' => TRUE));
           // $nodeurl = Url::fromRoute('entity.node.canonical', ['node' => $nid])->toString();
          $documentXml->addChild('URI',$requstURL . $node->toUrl()->toString());
-         $datatype = $node->type->entity->label();
+         $datatype = $node->type->entity->label(); 
          switch (strtolower($datatype)) {
            case "page de base":
            case "basic page":
            case "page de destination":
            case "landing page":
+           case "page interne":
             $datatype = "gene-gene";
             break;
            case "titre du poste":
-           case "employment opportunity":
+           case "employment opportunity": 
+           case "opportunité d'emploi":
 
              $datatype = "empl-empl";
              break;
@@ -86,6 +88,7 @@ class idol_feed_api_Controller extends ControllerBase {
                 break;
               case "dcterms_description":
                 $dcterms_description = $value["#attributes"]["content"];
+                $dcterms_description = htmlentities($dcterms_description, ENT_XML1, 'UTF-8');
                 break;
               case "keywords":
                 $keywords = $value["#attributes"]["content"];
@@ -159,6 +162,7 @@ class idol_feed_api_Controller extends ControllerBase {
                  break;
                case "dcterms_description":
                  $dcterms_description = $value["#attributes"]["content"];
+                 $dcterms_description = htmlentities($dcterms_description, ENT_XML1, 'UTF-8');
                  break;
                case "keywords":
                  $keywords = $value["#attributes"]["content"];
