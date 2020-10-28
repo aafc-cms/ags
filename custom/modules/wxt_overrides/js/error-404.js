ab7fdaae688c8ddd5ce7c6b2efc78070cd9a8962
olstjos <j@7pro.ca>
2020-10-28 12:51:21 -0400

    agrcms/d8#287 - Ensure non-node pages get basic metatags, fix . to : for a metatag or two , add a js file for error404 page.

---
+++ custom/modules/wxt_overrides/js/error-404.js
@@ -0,0 +1,58 @@
+/**
+ * @file
+ * WxT Overrides behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.wxtOverrides = {
+    attach: function (context, settings) {
+      if (context == document) {
+        Error404.init();
+      }
+    }
+  };
+
+} (jQuery, Drupal));
+
+var Error404 = function() {
+  var initialized = false;   // Flag to indicate that this class has been initialized
+
+  /**
+   * Initialization
+   */
+  function init() {
+    if (initialized) {
+      return;
+    }
+    Agrisource.init();
+
+    // Get the current UI language
+    console.log('Error 404');
+    console.log('Erreur 404!');
+    if (Agrisource.lang == 'en') {
+      document.title = "We couldn't find that Web page (Error 404) - Agrisource";
+      $('head meta[property*="title"]').remove();
+      $('head').prepend('<meta property="dcterms:title" content="404 Error - Page not found">');
+    }
+    else {
+      $('head meta[property*="title"]').remove();
+      document.title = "Nous ne pouvons trouver cette page Web (Erreur 404) - Agrisource";
+      $('head').prepend('<meta property="dcterms:title" content="Erreur 404 - Page non trouvée" lang="fr">');
+    }
+    $('head').prepend('<meta property="robots" content="noindex, nofollow, noarchive">');
+  }
+
+  /**
+   * Expose functions and variables
+   */
+  return {
+    init: init,
+  }
+}();
+
