3485916dfac51823b5e228481036328342e59bcc
olstjos <j@7pro.ca>
2020-11-02 15:17:48 -0500

    agrcms/d8#316 - Uncomment logging.

--- custom/modules/wxt_overrides/src/Controller/System4xxOverride.php
+++ custom/modules/wxt_overrides/src/Controller/System4xxOverride.php
@@ -131,7 +131,7 @@ class System4xxOverride extends ControllerBase implements ContainerInjectionInte
     }
     else {
       $request_uri = \Drupal::request()->getPathInfo();
-      \Drupal\agri_admin\AgriAdminHelper::addToLog($request_uri, TRUE);
+      //\Drupal\agri_admin\AgriAdminHelper::addToLog($request_uri, TRUE);
       if ($request_uri == '/agrisource/index.jsp') {
         Utils::gotoLegacy('<front>', [], '301'); // @TODO , find the route for this.
       }
