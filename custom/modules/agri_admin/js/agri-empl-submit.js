a353a0a09b437bb6b3fda765c251d1a02e7af98d
olstjos <j@7pro.ca>
2020-11-16 13:19:07 -0500

    agrcms/d8#336 - Fix type-o in form validation for employment submission form.

--- custom/modules/agri_admin/js/agri-empl-submit.js
+++ custom/modules/agri_admin/js/agri-empl-submit.js
@@ -14,18 +14,18 @@
     attach: function (context, settings) {
       if ($('.block-formblock').length && $('#edit-preview').length) {
         // Only initialize when the formblock becomes available.
-        AgriHelper.init();
+        AgriEmplSubmitHelper.init();
       }
       else if ($('body').hasClass('node-add') &&
         $('#edit-preview').length ) {
-        AgriHelper.init();
+        AgriEmplSubmitHelper.init();
       }
     }
   };
 
 } (jQuery, Drupal));
 
-var AgriHelper = function() {
+var AgriEmplSubmitHelper = function() {
   var initialized = false;   // Flag to indicate that this class has been initialized
   var form_required_valid = true; // Flag to indicate that the validation is for the Empl (EO) forms
   var lang = 'en';           // Will be 'en' or 'fr' regardless of how the url segment is formed (currently eng or fra)
@@ -47,24 +47,24 @@ var AgriHelper = function() {
 
     //Determine the page type
     if ($('body').hasClass('nodeaddempl')) {
-      AgriHelper.page_type = 'add-empl-special';
+      AgriEmplSubmitHelper.page_type = 'add-empl-special';
     }
     if ($('body').hasClass('nodeaddempl')) {
-      AgriHelper.page_type = 'add-empl-special';
+      AgriEmplSubmitHelper.page_type = 'add-empl-special';
     }
     //Determine the page type
     if ($('body').hasClass('employment-opportunity-request-form-page')) {
-      AgriHelper.page_type = 'add-empl-special';
+      AgriEmplSubmitHelper.page_type = 'add-empl-special';
     }
     else if ($('body').hasClass('news-article-form-page')) {
-      AgriHelper.page_type = 'add-news-special';
+      AgriEmplSubmitHelper.page_type = 'add-news-special';
     }
 
     //added preview and submit button event handler
     $('#edit-preview').click(function(){ handlePreviewClickEvent();})
     $('#edit-submit').click(function(){ handleSubmitClickEvent();});
 
-    console.log(AgriHelper.page_type);
+    console.log(AgriEmplSubmitHelper.page_type);
 
     initialized = true;
   }
@@ -77,38 +77,40 @@ var AgriHelper = function() {
     validate empl (EO) form.
   */
   function formRequiredFieldsValidation() {
-    AgriHelper.checkbox_valid = false;
+    AgriEmplSubmitHelper.checkbox_valid = false;
     $('div[id="edit-field-type"]').children().find('input').each(function(index, element) {
       if ($(element).hasClass('form-checkbox')) {
-        if ( $(element).prop('checked') && (!AgriHelper.checkbox_valid)) {
-           AgriHelper.checkbox_valid = true;
+        if ( $(element).prop('checked') && (!AgriEmplSubmitHelper.checkbox_valid)) {
+           AgriEmplSubmitHelper.checkbox_valid = true;
         }
       }
     });
 
-    AgriHelper.form_required_valid =  AgriHelper.checkbox_valid;
-    console.log ( AgriHelper.checkbox_valid);
+    AgriEmplSubmitHelper.form_required_valid =  AgriEmplSubmitHelper.checkbox_valid;
+    console.log ( AgriEmplSubmitHelper.checkbox_valid);
     var errlbl = $('#edit-emplopptypes-0-value-error');
     var errlblexists  = errlbl.length;
-    if (!AgriHelper.checkbox_valid && !errlblexists) {
+    if (!AgriEmplSubmitHelper.checkbox_valid && !errlblexists) {
      // inject error label after the last checkbox to place close to the input field
       var errfieldlbl = ''; 
       if (lang == 'fr') {
-        errfieldlbl = '<label id="edit-emplopptypes-0-value-error" for="edit-emplopptypes-0-value-error" style="display: block; color: #a94442;">Type de possibilité di\'emploi field est requis.</label>';
-      } else {
+        errfieldlbl = '<label id="edit-emplopptypes-0-value-error" for="edit-emplopptypes-0-value-error" style="display: block; color: #a94442;">Type de possibilité d\'emploi est requis.</label>';
+      }
+      else {
         errfieldlbl = '<label id="edit-emplopptypes-0-value-error" for="edit-emplopptypes-0-value-error" style="display: block; color: #a94442;">Employment Opportunity Type(s) field is required.</label>';
       }
       $('div#edit-field-type[class="form-checkboxes"] div').last().after(errfieldlbl);
       // make the validation error be close to the input field
       $('div#edit-field-type[class="form-checkboxes"] div').last().css({"margin-bottom": "0px"});
-    } else
-    {
-      if (AgriHelper.checkbox_valid && errlblexists) {
+    }
+    else {
+      if (AgriEmplSubmitHelper.checkbox_valid && errlblexists) {
         // hide the message
          $('#edit-emplopptypes-0-value-error').hide();
          $('div#edit-field-type[class="form-checkboxes"] div').last().css({"margin-bottom": "10px"});
-      } else {
-        if (!AgriHelper.checkbox_valid && errlblexists) {
+      }
+      else {
+        if (!AgriEmplSubmitHelper.checkbox_valid && errlblexists) {
         //we need to bring it back
           $('#edit-emplopptypes-0-value-error').show();
           $('div#edit-field-type[class="form-checkboxes"] div').last().css({"margin-bottom": "0px"});
@@ -116,13 +118,13 @@ var AgriHelper = function() {
       }
     }
 
-    //return AgriHelper.form_required_valid;
+    //return AgriEmplSubmitHelper.form_required_valid;
 
   /*$('select[required="required"]').each(function(index, element) {
       if ($(element).hasClass('form-select')) {
         if ($(element).val() == '_none') {
-          AgriHelper.form_required_valid = false;
-          return AgriHelper.form_required_valid;
+          AgriEmplSubmitHelper.form_required_valid = false;
+          return AgriEmplSubmitHelper.form_required_valid;
         }
       }
     });
