3dce0d630b3603fca8d52502abd7c2588e6c2709
olstjos <j@7pro.ca>
2022-02-18 10:37:00 -0500

    agrcms/d8#580 - Error proof the drush command so that it doesn't crash Simons setups agrcms/d8#593.

--- post_install.sh
+++ post_install.sh
@@ -330,3 +330,16 @@ fi
 #fi
 
 echo "**** End behatags setup/verifications ****"
+
+drush status --field=Database > test-connection.txt || true; # Ignore errors.
+
+if grep -q "Connected" test-connection.txt; then
+  echo "Connected to the database, setting up the db views now.";
+  #Run Create or Replace View sql command
+  echo  "Creating view: drush sql-query --file=../custom/dbviews/search_node_url.sql";
+                        drush sql-query --file=../custom/dbviews/search_node_url.sql;
+else
+  echo "The database is not yet configured, cannot install the db views at this time.";
+fi
+rm test-connection.txt;
+
--- post_install_view.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-#Run Create or Replace View sql command
-echo  "Creating view: drush sql-query --file=../custom/dbviews/search_node_url.sql";
-drush sql-query --file=../custom/dbviews/search_node_url.sql;
