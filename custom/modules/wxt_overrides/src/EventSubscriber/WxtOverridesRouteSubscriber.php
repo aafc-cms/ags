3eb0685bd11d985b8f87714ec6d0bed954e71393
olstjos <j@7pro.ca>
2023-05-16 04:47:51 +0000

    agrcms/d8#790 - Make wxt_overrides compatible with Drupal 10.

--- custom/modules/wxt_overrides/src/EventSubscriber/WxtOverridesRouteSubscriber.php
+++ custom/modules/wxt_overrides/src/EventSubscriber/WxtOverridesRouteSubscriber.php
@@ -26,7 +26,7 @@ class WxtOverridesRouteSubscriber extends RouteSubscriberBase {
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents() : array {
     $events = parent::getSubscribedEvents();
 
     // Use a lower priority than \Drupal\views\EventSubscriber\RouteSubscriber
