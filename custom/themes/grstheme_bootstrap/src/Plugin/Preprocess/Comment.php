b24c3c98ec2ca108f561010fd18e9a6410585ded
olstjos <j@7pro.ca>
2023-06-19 23:49:46 +0000

    agrcms/d8#225 - Blog for D10, with comment owner name.

---
+++ custom/themes/grstheme_bootstrap/src/Plugin/Preprocess/Comment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\grstheme_bootstrap\Plugin\Preprocess;
+
+use Drupal\bootstrap\Plugin\Preprocess\PreprocessBase;
+use Drupal\bootstrap\Utility\Variables;
+
+/**
+ * Implements hook_form_FORM_ID_alter().
+ *
+ * @ingroup plugins_form
+ *
+ * @BootstrapPreprocess("comment")
+ */
+class Comment extends PreprocessBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocessVariables(Variables $variables) {
+    $variables['author'] = \Drupal::service('renderer')->render($elements);
+    if (empty($variables['author']) && $variables['comment']->getOwner()->id() === 0) {
+      if ($variables['comment']->hasField('field_my_name')) {
+        $user = $variables['comment']->get('field_my_name');
+	$variables['author'] = $user->value;
+      }
+    }
+    if (empty($variables['author'])) {
+      $variables['author'] = $variables['comment']->getAuthorName();
+    }
+
+    // Getting the node creation time stamp from the comment object.
+    $date = $variables['comment']->getCreatedTime();
+
+    // Adjust submitted display.
+    $variables['created'] = \Drupal::service('date.formatter')->format($date, 'wxt_standard');
+    $variables['submitted'] = $this->t('@username - <span class="comments-ago">@datetime </span>', [
+      '@username' => $variables['author'],
+      '@datetime' => $variables['created'],
+    ]);
+  }
+
+}
