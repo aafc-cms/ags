3e2ead99404aa4c0a98453449dd920a09822f7bd
olstjos <j@7pro.ca>
2023-06-20 23:37:27 +0000

    agrcms/d8#225 - Add configuration options for blog.

--- custom/modules/wxt_overrides/src/Form/SettingsForm.php
+++ custom/modules/wxt_overrides/src/Form/SettingsForm.php
@@ -28,8 +28,48 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $blog_hide_link = $this->config('wxt_overrides.settings')->get('blog_hide_link');
+    if (is_null($blog_hide_link)) {
+      $blog_hide_link = FALSE;
+    }
+    $blog_hide_link = (integer) $blog_hide_link;
+    $form['blog_hide_link'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide blog link.'),
+      '#default_value' => $blog_hide_link,
+    ];
+    $blog_hide_save = $this->config('wxt_overrides.settings')->get('blog_hide_save');
+    if (is_null($blog_hide_save)) {
+      $blog_hide_save = FALSE;
+    }
+    $blog_hide_save = (integer) $blog_hide_save;
+    $form['blog_hide_save'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide blog comment save button.'),
+      '#default_value' => $blog_hide_save,
+    ];
+    $blog_hide_tags = $this->config('wxt_overrides.settings')->get('blog_hide_tags');
+    if (is_null($blog_hide_tags)) {
+      $blog_hide_tags = FALSE;
+    }
+    $blog_hide_tags = (integer) $blog_hide_tags;
+    $form['blog_hide_tags'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide blog tags.'),
+      '#default_value' => $blog_hide_tags,
+    ];
+    $blog_anon = $this->config('wxt_overrides.settings')->get('blog_anon');
+    if (is_null($blog_anon)) {
+      $blog_anon = FALSE;
+    }
+    $blog_anon = (integer) $blog_anon;
+    $form['blog_anon'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Anonymous role blog comments only.'),
+      '#default_value' => $blog_anon,
+    ];
     $form['example'] = [
-      '#type' => 'textfield',
+      '#type' => 'hidden',
       '#title' => $this->t('Example'),
       '#default_value' => $this->config('wxt_overrides.settings')->get('example'),
     ];
@@ -52,6 +92,10 @@ class SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('wxt_overrides.settings')
       ->set('example', $form_state->getValue('example'))
+      ->set('blog_hide_link', $form_state->getValue('blog_hide_link'))
+      ->set('blog_hide_save', $form_state->getValue('blog_hide_save'))
+      ->set('blog_hide_tags', $form_state->getValue('blog_hide_tags'))
+      ->set('blog_anon', $form_state->getValue('blog_anon'))
       ->save();
     parent::submitForm($form, $form_state);
   }
