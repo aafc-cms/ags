7ced6faf2b40752a033b821f0773dd7027e938b9
olstjos <j@7pro.ca>
2023-08-03 02:51:50 +0000

    agrcms/d8#816 - Script that identifies duplicate files.

---
+++ unique_script.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *  Return array of fid.
+ **/
+function getDuplicateFids() {
+  $database = \Drupal::database();
+  $result = $database->query('select fid from filehash group by sha256 having count(sha256)')
+    ->fetchAllAssoc('fid', \PDO::FETCH_ASSOC);
+  $fids = array_keys($result);
+  return $fids;
+}
+
+$fids = getDuplicateFids();
+foreach ($fids as $fid) {
+  $file = \Drupal\file\Entity\File::load($fid);
+  echo "Duplicate file: " . $file->getFileUri();
+  echo "\n";
+}
