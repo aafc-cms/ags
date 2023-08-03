b9df3b5d565992117f346ee32257adaa1558fa7e
olstjos <j@7pro.ca>
2023-08-03 02:54:50 +0000

    agrcms/d8#816 - Add a total at the bottom, having > 1, this works.

--- unique_script.php
+++ unique_script.php
@@ -5,15 +5,19 @@
  **/
 function getDuplicateFids() {
   $database = \Drupal::database();
-  $result = $database->query('select fid from filehash group by sha256 having count(sha256)')
+  $result = $database->query('select fid from filehash group by sha256 having count(sha256) > 1')
     ->fetchAllAssoc('fid', \PDO::FETCH_ASSOC);
   $fids = array_keys($result);
   return $fids;
 }
 
 $fids = getDuplicateFids();
+
 foreach ($fids as $fid) {
   $file = \Drupal\file\Entity\File::load($fid);
   echo "Duplicate file: " . $file->getFileUri();
   echo "\n";
 }
+
+
+echo count($fids) . ' duplicates in total';
