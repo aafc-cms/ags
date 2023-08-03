c30840426467747e090497159053dfcdec57b2b5
olstjos <j@7pro.ca>
2023-08-03 05:31:21 +0000

    agrcms/d8#816 - Improve the script to make the output more useful.

--- unique_script.php
+++ unique_script.php
@@ -1,6 +1,29 @@
 <?php
 
 /**
+ *  Return sha256 for fid.
+ **/
+function getHashByFid($fid) {
+  $database = \Drupal::database();
+  $result = $database->query('select sha256 from filehash where fid = :fid', [ ':fid' => $fid ])
+    ->fetchAllAssoc('sha256', \PDO::FETCH_ASSOC);
+  $sha256s = array_keys($result);
+  $sha256 = reset($sha256s);
+  return $sha256;
+}
+
+/**
+ *  Return all fids for sha256 value.
+ **/
+function getFidsBySha256($sha256) {
+  $database = \Drupal::database();
+  $result = $database->query('select fid from filehash where sha256 = :sha256', [ ':sha256' => $sha256 ])
+    ->fetchAllAssoc('fid', \PDO::FETCH_ASSOC);
+  $fids = array_keys($result);
+  return $fids;
+}
+
+/**
  *  Return array of fid.
  **/
 function getDuplicateFids() {
@@ -14,8 +37,20 @@ function getDuplicateFids() {
 $fids = getDuplicateFids();
 
 foreach ($fids as $fid) {
-  $file = \Drupal\file\Entity\File::load($fid);
-  echo "Duplicate file: " . $file->getFileUri();
+  $sha256 = getHashByFid($fid);
+  $fids_with_matches = getFidsBySha256($sha256); 
+  echo "****** sha256 = $sha256 *****";
+  echo "\n";
+  foreach ($fids_with_matches as $fid_matched) {
+    echo "        fid = $fid_matched";
+    echo "\n";
+  }
+  foreach ($fids_with_matches as $fid_matched) {
+    $file = \Drupal\file\Entity\File::load($fid_matched);
+    echo "Is duplicate= " . $file->getFileUri();
+    echo "\n";
+  }
+  echo "*****************************";
   echo "\n";
 }
 
